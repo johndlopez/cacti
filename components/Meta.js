e66f7c8a88b216bcc83cdd49860ee5938fb38ab9
johndlopez <johnlopez@Johns-MacBook-Pro-2.local>
2018-12-10 13:08:23 -0800

    added preliminary styling

---
+++ components/Meta.js
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+const Meta = () => (
+  <Head>
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta charSet="utf-8" />
+    {/* <link rel="shortcut icon" href="/static/favicon.png" /> */}
+    <link
+      href="https://fonts.googleapis.com/css?family=Gamja+Flower|Lato"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+    <title>Sick Fits!</title>
+  </Head>
+);
+
+export default Meta;
--- components/Page.js
+++ components/Page.js
@@ -1,13 +1,59 @@
 import React, { Component } from 'react';
+import styled, { ThemeProvider, injectGlobal } from 'styled-components';
 import Header from './Header';
+import Meta from './Meta';
+
+const theme = {
+  black: '#131313',
+  green: '#79A182',
+  lightGrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)'
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${props => props.theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${props => props.theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
+
+injectGlobal`
+  html {
+    box-sizing: border-box;
+    font-size: 10px;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
+  body {
+    padding: 0;
+    margin: 0;
+    font-size: 1.5rem;
+    line-height: 2;
+    font-family: 'Lato', sans-serif;
+  }
+  a {
+    text-decoration: none;
+    color: ${theme.black};
+  }
+`;
 
 export default class Page extends Component {
   render() {
     return (
-      <div>
-        <Header />
-        <div>Page</div>
-      </div>
+      <ThemeProvider theme={theme}>
+        <StyledPage>
+          <Meta />
+          <Header />
+          <Inner>{this.props.children}</Inner>
+        </StyledPage>
+      </ThemeProvider>
     );
   }
 }
