a05864f8792b2dca53b0a0c111505d22211e2010
johndlopez <johnlopez@Johns-MacBook-Pro-2.local>
2018-12-10 13:40:50 -0800

    preliminary nav styling

--- components/Header.js
+++ components/Header.js
@@ -39,7 +39,7 @@ const StyledHeader = styled.header`
 `;
 
 const Header = () => (
-  <div>
+  <StyledHeader>
     <div className="bar">
       <Logo>
         <Link href="/">
@@ -52,7 +52,7 @@ const Header = () => (
       <p>Search</p>
     </div>
     <div>Cart</div>
-  </div>
+  </StyledHeader>
 );
 
 export default Header;
--- components/Nav.js
+++ components/Nav.js
@@ -1,3 +1,18 @@
-const Nav = () => <div>Nav</div>;
+import Link from 'next/link';
+import NavStyles from './styles/NavStyles';
+
+const Nav = () => (
+  <NavStyles>
+    <Link href="/featured">
+      <a>featured</a>
+    </Link>
+    <Link href="/account">
+      <a>account</a>
+    </Link>
+    <Link href="/cart">
+      <a>cart</a>
+    </Link>
+  </NavStyles>
+);
 
 export default Nav;
