007fed8ee8e4c4d0f961373d2cc54abc4bd298c7
johndlopez <johnlopez@Johns-MacBook-Pro-2.local>
2018-12-10 13:54:55 -0800

    made nav bar responsive

--- components/styles/NavStyles.js
+++ components/styles/NavStyles.js
@@ -1,11 +1,38 @@
 import styled from 'styled-components';
 
 const NavStyles = styled.ul`
-  margin: 0;
+  margin: 0 4rem 0 0;
   padding: 0;
   display: flex;
   justify-self: end;
   font-size: 2rem;
+  a,
+  button {
+    padding: 1rem 3rem;
+    display: flex;
+    align-items: center;
+    position: relative;
+    font-size: 1em;
+    border: 0;
+    background: none;
+    cursor: pointer;
+    @media (max-width: 700px) {
+      font-size: 10px;
+      padding: 0 10px;
+    }
+    &:hover,
+    &:focus {
+      outline: none;
+      &:after {
+        width: calc(100% - 60px);
+      }
+    }
+  }
+  @media (max-width: 1300px) {
+  width: 100%;
+  justify-content: center;
+  font-size: 1.5rem;
+  margin: 0;
 `;
 
 export default NavStyles;
